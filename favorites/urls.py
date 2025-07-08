5c0f4ea0f92851459dec70d0286bc050f8cc735a
leehyunro123 <lee010320123@gmail.com>
2025-07-08 21:34:04 +0900

    여름방학 1주차 과제 수정

--- favorites/urls.py
+++ favorites/urls.py
@@ -7,4 +7,5 @@ app_name = 'favorites'
 urlpatterns = [
     path('songs/', views.songs_list, name='songs_list'),
     path('singers/', views.singer_list, name='singer_list'),
+    path('singers/<int:singer_id>/', views.singer_detail, name='singer_detail'),
     ]
--- favorites/views.py
+++ favorites/views.py
@@ -37,3 +37,23 @@ def singer_list(request):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
         return response.Response(data=serializer.data)
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def singer_detail(request, singer_id):
+    singer = get_object_or_404(Singers, id=singer_id)
+    
+    if request.method == 'GET':
+        serializer = SingerSerializer(singer)
+        return response.Response(data=serializer.data)
+    
+    elif request.method == 'PATCH':
+        serializer = SingerSerializer(singer, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(data=serializer.data)
+        return response.Response(data=serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        singer_id = singer.id
+        singer.delete()
+        return response.Response(data={'id': singer_id, 'message': '가수가 성공적으로 삭제되었습니다.'})
